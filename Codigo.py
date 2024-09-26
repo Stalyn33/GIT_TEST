236c4c8fe6022db40c80c639dcf0762425a1bbca
Washington Alvarez <w224189@dac.unicamp.br>
2024-09-26 20:47:33 -0300

    Nuevos DOCS

---
+++ Codigo.py
@@ -0,0 +1,14 @@
+print("Hola, mundo!")
+
+a = 5
+b = 10
+suma = a + b
+producto = a * b
+
+print(f"La suma de {a} y {b} es {suma}")
+print(f"El producto de {a} y {b} es {producto}")
+
+if suma > producto:
+    print("La suma es mayor que el producto")
+else:
+    print("El producto es mayor o igual que la suma")
